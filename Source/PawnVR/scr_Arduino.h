dedc2196dae481036b69b78e74d11d7bd44d3775
sbasballes <89862588+sbasballes@users.noreply.github.com>
2021-08-31 19:28:58 +0200

    Create scr_Arduino.h

---
+++ Source/PawnVR/scr_Arduino.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PAWNVR_API scr_Arduino
+{
+public:
+	scr_Arduino();
+	~scr_Arduino();
+};
